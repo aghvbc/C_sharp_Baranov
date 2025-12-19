d814cd3eff63b896912785b6e9b69f97f02ec645
aghvbc <barantaran2007@gmail.com>
2025-12-19 21:30:47 +0300

    uncomment code

--- practice_working_with_strings/Taskhard1_ToU_ToL.cs
+++ practice_working_with_strings/Taskhard1_ToU_ToL.cs
@@ -10,45 +10,45 @@
 // "Антон Петров-Иванов"
 
 
-// using System;
-// using System.Data.SqlTypes;
-// using System.Text;
+using System;
+using System.Data.SqlTypes;
+using System.Text;
 
-// class Program
-// {
-//     static void Main()
-//     {
-//         string mystring = "АнТоН pOn John-ИваНОВ";
+class Program
+{
+    static void Main()
+    {
+        string mystring = "АнТоН pOn John-ИваНОВ";
 
-//         bool new_word = true;
-//         StringBuilder res = new StringBuilder();
+        bool new_word = true;
+        StringBuilder res = new StringBuilder();
         
         
-//         foreach (char letter in mystring)
-//         {
-//             if (char.IsLetter(letter))
-//             {
-//                 if (new_word)
-//                 {
-//                     res.Append(char.ToUpper(letter));
-//                     new_word = false;
+        foreach (char letter in mystring)
+        {
+            if (char.IsLetter(letter))
+            {
+                if (new_word)
+                {
+                    res.Append(char.ToUpper(letter));
+                    new_word = false;
 
-//                 }
-//                 else
-//                 {
-//                     res.Append(char.ToLower(letter));
-//                 }
+                }
+                else
+                {
+                    res.Append(char.ToLower(letter));
+                }
 
-//             }
-//             else
-//             {
-//                 res.Append(letter);
-//                 new_word = true;
+            }
+            else
+            {
+                res.Append(letter);
+                new_word = true;
 
-//             }
+            }
 
-//         }
-//         Console.WriteLine(res.ToString());
-//     }
+        }
+        Console.WriteLine(res.ToString());
+    }
 
-// }
+}
--- practice_working_with_strings/Taskhard2_Regex.cs
+++ practice_working_with_strings/Taskhard2_Regex.cs
@@ -12,60 +12,60 @@
 // Line 24: INFO Finish
 // Должно вернуть строки 20–24.
 
-// using System;
-// using System.Text.RegularExpressions;
+using System;
+using System.Text.RegularExpressions;
 
-// class Program
-// {
-//     static void Main()
-//     {
-//         string log = """
-//             Line 001: DEBUG Starting application...
-//             Line 002: INFO Loading configuration
-//             Line 003: INFO Database connection established
-//             Line 004: WARN Deprecated API used
-//             Line 005: INFO User login: admin
-//             Line 006: INFO Session created
-//             Line 007: ERROR Failed to connect to payment gateway
-//             Line 008: ERROR Timeout after 5000ms
-//             Line 009: ERROR Transaction aborted
-//             Line 010: INFO Sending alert to administrator
-//             Line 011: INFO System recovery initiated
-//             Line 012: INFO Application restarted successfully
-//             Line 013: DEBUG Cache cleared
-//             Line 014: INFO User login: guest
-//             Line 015: ERROR Division by zero in module Calculator
-//             Line 016: WARN High memory usage detected
-//             Line 017: INFO Cleanup completed
-//             Line 018: DEBUG End of log 
-//            """;
+class Program
+{
+    static void Main()
+    {
+        string log = """
+            Line 001: DEBUG Starting application...
+            Line 002: INFO Loading configuration
+            Line 003: INFO Database connection established
+            Line 004: WARN Deprecated API used
+            Line 005: INFO User login: admin
+            Line 006: INFO Session created
+            Line 007: ERROR Failed to connect to payment gateway
+            Line 008: ERROR Timeout after 5000ms
+            Line 009: ERROR Transaction aborted
+            Line 010: INFO Sending alert to administrator
+            Line 011: INFO System recovery initiated
+            Line 012: INFO Application restarted successfully
+            Line 013: DEBUG Cache cleared
+            Line 014: INFO User login: guest
+            Line 015: ERROR Division by zero in module Calculator
+            Line 016: WARN High memory usage detected
+            Line 017: INFO Cleanup completed
+            Line 018: DEBUG End of log 
+           """;
 
 
-//         // Разбор паттерна:
-//         // (?<=                  - Начало Positive Lookbehind (просмотр назад)
-//         //   (?<pre>             - Именованная группа "pre" для захвата строк ДО
-//         //     (?:.*\r?\n){0,2}  - От 0 до 2 строк (нежадный захват строк с переносами)
-//         //   )
-//         // )
-//         // .*ERROR.*             - Основное совпадение: строка, содержащая ERROR
-//         // (?=                   - Начало Positive Lookahead (просмотр вперед)
-//         //   (?<post>            - Именованная группа "post" для захвата строк ПОСЛЕ
-//         //     (?:\r?\n.*){0,2}  - От 0 до 2 строк (начиная с переноса строки)
-//         //   )
-//         // )
+        // Разбор паттерна:
+        // (?<=                  - Начало Positive Lookbehind (просмотр назад)
+        //   (?<pre>             - Именованная группа "pre" для захвата строк ДО
+        //     (?:.*\r?\n){0,2}  - От 0 до 2 строк (нежадный захват строк с переносами)
+        //   )
+        // )
+        // .*ERROR.*             - Основное совпадение: строка, содержащая ERROR
+        // (?=                   - Начало Positive Lookahead (просмотр вперед)
+        //   (?<post>            - Именованная группа "post" для захвата строк ПОСЛЕ
+        //     (?:\r?\n.*){0,2}  - От 0 до 2 строк (начиная с переноса строки)
+        //   )
+        // )
         
-//         string pattern = @"(?<=(?<pre>(?:.*\r?\n){0,2})).*ERROR.*(?=(?<post>(?:\r?\n.*){0,2}))";
+        string pattern = @"(?<=(?<pre>(?:.*\r?\n){0,2})).*ERROR.*(?=(?<post>(?:\r?\n.*){0,2}))";
 
-//         Regex regex = new Regex(pattern, RegexOptions.Multiline);
+        Regex regex = new Regex(pattern, RegexOptions.Multiline);
 
-//         MatchCollection matches = regex.Matches(log);
+        MatchCollection matches = regex.Matches(log);
 
 
-//         foreach (Match m in matches)
-//         {
-//             string contextBlock = m.Groups["pre"].Value + m.Value + m.Groups["post"].Value;
+        foreach (Match m in matches)
+        {
+            string contextBlock = m.Groups["pre"].Value + m.Value + m.Groups["post"].Value;
 
-//             Console.WriteLine(contextBlock.Trim());
-//         }
-//     }
-// }
+            Console.WriteLine(contextBlock.Trim());
+        }
+    }
+}
